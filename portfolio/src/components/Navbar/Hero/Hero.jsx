ac0cac7eee36021089152a42d1d9afd68da63e5a
joshbatties <jbat0018@student.monash.edu>
2024-04-03 04:57:55 +1100

    create hero section on the homepag

--- portfolio/src/components/Navbar/Hero/Hero.jsx
+++ portfolio/src/components/Navbar/Hero/Hero.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+
+import styles from "./Hero.module.css";
 import { getImageUrl } from "../../../utils";
 
 export const Hero = () => {
-    return <section>
-        <div>
-            <h1>Hi, I'm Josh</h1>
-            <p>
-                I'm a third year Commerce/Computer Science student at Monash University. This is my portfolio website to display my projects. Please contact me if you'd like to learn more!
+    return (
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, I'm Josh</h1>
+            <p className={styles.description}>
+                This is my portfolio website to display my projects. Please contact me if you'd like to learn more!
             </p>
-            <a href="mailto:joshbatties.dev@gmail.com">Contact me</a>
+            <a href="mailto:joshbatties.dev@gmail.com" className={styles.contactBtn}>Contact me</a>
         </div>
-        <img src={getImageUrl("hero/heroImage.png")} alt= "Hero image of me"/>
-    </section>;
+        <img src={getImageUrl("hero/heroImage.png")} alt= "Hero image of me" className={styles.heroImg}/>
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+
+    </section>
+    );
 };
