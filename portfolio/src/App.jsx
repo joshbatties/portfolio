9cdb7600731328067276df45e2f6f20ed1558367
joshbatties <jbat0018@student.monash.edu>
2024-04-03 01:46:52 +1100

    Added Navbar

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,10 @@
+import styles from './App.modules.css';
+import { Navbar } from './components/Navbar/Navbar';
+function App() {
+  return (
+    <div className={styles.App}>
+      <Navbar />
+    </div>);
+}
+
+export default App
