444ac40542c7a16bffe6f3b4a04ca8534fd6f528
joshbatties <jbat0018@student.monash.edu>
2024-04-03 03:00:11 +1100

    Navbar ready
    Need to import png files for Navbar icon

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,12 @@
-import styles from './App.modules.css';
+import styles from './App.module.css'; 
 import { Navbar } from './components/Navbar/Navbar';
+
 function App() {
   return (
-    <div className={styles.App}>
-      <Navbar />
-    </div>);
+   <div className={styles.App}>
+    <Navbar />
+  </div>
+  );
 }
 
-export default App
+export default App;
--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
-export const NavBar = () => {
-    return <div>Navbar</div>;
+import styles from "./Navbar.module.css";
+import {getImageUrl} from "../../utils";
+
+export const Navbar = () => {
+    return (
+    <nav className={styles.navbar}>
+       <a className={styles.title} href="/">
+        Home
+        </a>
+       <div className= {styles.menu}>
+        <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")}/>
+        <ul className= {styles.menuItems}>
+            <li>
+                <a href="#about">About me</a>
+            </li>
+            <li>
+                <a href="#projects">My Projects</a>
+            </li>
+            <li>
+                <a href="#videos">My Videos</a>
+            </li>
+            <li>
+                <a href="#contact">Contact me</a>
+            </li>
+        </ul>
+       </div>
+    </nav>
+    );
 };
