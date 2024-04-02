9a9f67e2c33b676be490b74bcccda6c5046e5db4
joshbatties <jbat0018@student.monash.edu>
2024-04-03 03:48:15 +1100

    finished Navbar

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -22,7 +22,10 @@ export const Navbar = () => {
             alt="menu-button"
             onClick={() => setMenuOpen(!menuOpen)}
         />
-        <ul className= {styles.menuItems}>
+        <ul 
+            className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+            onClick= {() => setMenuOpen(false)}
+        >
             <li>
                 <a href="#about">About me</a>
             </li>
