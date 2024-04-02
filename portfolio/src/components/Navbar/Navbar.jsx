0f7a1c9afb11472e840817e4cf3a6de534bb098f
joshbatties <jbat0018@student.monash.edu>
2024-04-03 03:31:53 +1100

    Update Navbar

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -4,13 +4,24 @@ import styles from "./Navbar.module.css";
 import {getImageUrl} from "../../utils";
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
     <nav className={styles.navbar}>
        <a className={styles.title} href="/">
         Home
         </a>
        <div className= {styles.menu}>
-        <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")}/>
+        <img 
+            className={styles.menuBtn} 
+            src={
+                menuOpen 
+                ? getImageUrl("nav/closeIcon.png") 
+                : getImageUrl("nav/menuIcon.png")
+            }
+            alt="menu-button"
+            onClick={() => setMenuOpen(!menuOpen)}
+        />
         <ul className= {styles.menuItems}>
             <li>
                 <a href="#about">About me</a>
