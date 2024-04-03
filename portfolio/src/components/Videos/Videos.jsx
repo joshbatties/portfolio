712121a43772bc3d88c5b8f98daa227ac97721ec
joshbatties <jbat0018@student.monash.edu>
2024-04-03 11:31:27 +1100

    Implemeted scroll-to feature for my videos

--- portfolio/src/components/Videos/Videos.jsx
+++ portfolio/src/components/Videos/Videos.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
+import styles from "./Videos.module.css";
+
 import videos from "../../data/videos.json";
 import { VideoCard } from "./VideoCard";
 
 export const Videos = () => {
     return (
-        <section>
-            <h2>Videos</h2>
-            <div>
+        <section className={styles.container} id = "videos">
+            <h2 className={styles.title}>Videos</h2>
+            <div className={styles.videos}>
                 {videos.map((video, id) => {
                     return <VideoCard key={id} video={video} />;
                 })}
